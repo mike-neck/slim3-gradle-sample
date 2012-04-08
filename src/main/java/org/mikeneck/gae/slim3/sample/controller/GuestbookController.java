a9b3dbb0bf311e15c5cde0286f7bb3db6014e27a
mike_neck <jkrt3333@gmail.com>
2012-04-09 05:51:25 +0900

    add and pass GuestbookController

--- src/main/java/org/mikeneck/gae/slim3/sample/controller/GuestbookController.java
+++ src/main/java/org/mikeneck/gae/slim3/sample/controller/GuestbookController.java
@@ -1,8 +1,49 @@
 package org.mikeneck.gae.slim3.sample.controller;
 
+import org.slim3.controller.Controller;
+import org.slim3.controller.Navigation;
+
+import java.io.PrintWriter;
+
 /**
  * @author: mike
  * @since: 12/04/09
  */
-public class GuestbookController {
+public class GuestbookController extends Controller {
+
+    private static final String TYPE = "text/html";
+    private static final String UTF_8 = "utf-8";
+
+    @Override
+    protected Navigation run() throws Exception {
+        response.setContentType(TYPE);
+        response.setCharacterEncoding(UTF_8);
+
+        PrintWriter writer = response.getWriter();
+        println(writer, "<html>")
+                .println(writer, "<head>")
+                .println(writer, "<title>")
+                .println(writer, "ゲストブック")
+                .println(writer, "</title>")
+                .println(writer, "</head>")
+                .println(writer, "<body>")
+                .println(writer, "<h1>なにかよろ！</h1>")
+                .println(writer, "<div>")
+                .println(writer, "<form action='/guestbook' method='post'>")
+                .println(writer, "<p>")
+                .println(writer, "<input type='text' name='message' id='message' size='50'/>")
+                .println(writer, "<input type='submit'/>")
+                .println(writer, "</p>")
+                .println(writer, "</form>")
+                .println(writer, "</div>")
+                .println(writer, "</body>")
+                .println(writer, "</html>");
+        response.flushBuffer();
+        return null;
+    }
+
+    private GuestbookController println(PrintWriter writer, String text) {
+        writer.println(text);
+        return this;
+    }
 }
--- src/test/java/org/mikeneck/gae/slim3/sample/controller/GuestbookControllerTest.java
+++ src/test/java/org/mikeneck/gae/slim3/sample/controller/GuestbookControllerTest.java
@@ -1,8 +1,53 @@
 package org.mikeneck.gae.slim3.sample.controller;
 
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mikeneck.gae.slim3.sample.util.ControllerTestRule;
+import org.slim3.tester.ControllerTester;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
+
 /**
- * @author: mike
- * @since: 12/04/09
+ * @author : mike
+ * @since : 12/04/09
  */
 public class GuestbookControllerTest {
+
+    @Rule
+    public static ControllerTestRule testRule = new ControllerTestRule();
+
+    private ControllerTester tester;
+
+    @Before
+    public void testSetUp () throws IOException, ServletException {
+        tester = testRule.getTester();
+        tester.request.setMethod("GET");
+        tester.start("/guestbook");
+    }
+
+    @Test
+    public void returns200() {
+        assertThat(testRule.getMethodName(), tester.response.getStatus(), is(HttpServletResponse.SC_OK));
+    }
+
+    @Test
+    public void assertsGuestbookController () {
+        assertThat(testRule.getMethodName(), tester.getController(), instanceOf(GuestbookController.class));
+    }
+
+    @Test
+    public void contentsIsNotNullAndBlank () throws IOException {
+        assertThat(testRule.getMethodName(), tester.response.getOutputAsString(), is(notNullValue()));
+    }
+
+    @Test
+    public void containsHtml () throws IOException {
+        assertThat(testRule.getMethodName(), tester.response.getOutputAsString(), containsString("<html"));
+    }
 }
